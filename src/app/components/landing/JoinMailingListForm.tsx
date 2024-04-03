0e1dfa365c738eaf59867da582d6474af7a691d7
Coach Chuck <christian.krueger.health@gmail.com>
2024-04-03 12:57:44 -0700

    video added

--- src/app/components/landing/JoinMailingListForm.tsx
+++ src/app/components/landing/JoinMailingListForm.tsx
@@ -4,19 +4,34 @@ import { SiteImage } from '@/app/models/images';
 import SVGIcon from '../SVGIcon';
 import { useFormState } from 'react-dom';
 import { addEmailToMailingList } from '@/app/(routes)/landing/actions';
+import { use, useEffect, useState } from 'react';
 
 export default function JoinMailingListForm() {
   const [state, formAction] = useFormState(addEmailToMailingList, { message: '' });
+  const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <div className="mx-6 mb-20 flex flex-col rounded-2xl border border-neutral-500 bg-gray-750 p-10 md:w-2/4 lg:w-1/3">
+  useEffect(() => {
+    setNotLoading();
+  }, [state]);
+
+  const setLoading = () => {
+    setIsLoading(true);
+  };
+  const setNotLoading = () => {
+    setIsLoading(false);
+  };
+
+  const renderForm = () => {
+    if (state.message) return null;
+
+    return <div>
       <h1 className="mb-2 text-center text-5xl text-stone-200">PREPARE</h1>
       <h1 className="mb-2 text-center text-5xl text-stone-200">FOR ARIVAL</h1>
       <p className="mb-6 text-center text-sm text-neutral-400">
         Sign up to receive upcoming notifications from The Super Phoenix DAO
       </p>
 
-      <form action={formAction}>
+      <form action={formAction} onSubmit={setLoading}>
         <input
           placeholder="name@example.com"
           className="mb-5 w-full appearance-none rounded-xl border border-neutral-500 bg-gray-850 p-4 leading-tight text-neutral-300"
@@ -25,13 +40,33 @@ export default function JoinMailingListForm() {
         />
         <button
           type="submit"
+          disabled={isLoading}
           className="mb-2 w-full appearance-none rounded-md border border-neutral-500 p-3 font-medium leading-tight text-neutral-300 hover:bg-gray-850 active:bg-gray-900"
         >
-          Join Mailing List
+          {isLoading ? 'Transmitting...' : 'Join Mailing List'}
         </button>
       </form>
+    </div>;
+  };
 
-      <p className="my-2 text-neutral-300 ">{state.message}</p>
+  const renderSuccessMessage = () => {
+    if (!state.message) return null;
+
+    return <div>
+      <h1 className="mb-2 text-center text-5xl text-stone-200">TRANSMISSION</h1>
+      <h1 className="mb-2 text-center text-5xl text-stone-200">COMPLETE</h1>
+      <p className="my-8 text-center text-neutral-300 ">{state.message}</p>
+
+    </div>
+
+  };
+
+  return (
+    <div className="mx-6 mb-20 flex flex-col rounded-2xl border border-neutral-500 bg-gray-750 p-10 md:w-2/4 lg:w-1/3">
+
+
+      {renderForm()}
+      {renderSuccessMessage()}
 
       <div className="flex justify-center">
         <SVGIcon iconPath={SiteImage.icon} alt="Logo" size="xxxl" />
--- src/app/models/links.ts
+++ src/app/models/links.ts
@@ -1,3 +1,4 @@
 export enum SiteLinks {
   twitter = 'https://twitter.com/SuperPhoenixDAO',
+  launchVideo = 'https://youtube.com/embed/h6CJYtY3tCw',
 }
