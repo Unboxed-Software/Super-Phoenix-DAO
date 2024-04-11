38d212bfa3d43767c2c81ea7c257d5ad70fad7e3
Coach Chuck <christian.krueger.health@gmail.com>
2024-04-11 11:10:37 -0600

    navbar added

---
+++ src/app/components/landing/Header.tsx
@@ -0,0 +1,58 @@
+import { SiteLinks } from "@/app/models/links";
+
+const Header = () => {
+  return (
+    <>
+      <header className="fixed top-0 z-10 w-full">
+        <nav className="container mx-auto px-6 py-3">
+          <div className="flex items-center justify-between">
+            <a href="#" className="text-2xl font-bold text-gray-800"></a>
+            <div className="hidden items-center space-x-4 text-lg md:flex">
+              <a href={SiteLinks.whitepaper} className=" hover:text-gray-400">
+                Whitepaper
+              </a>
+
+              <a href="#" className="cursor-default text-gray-400 tooltip">
+                NFT
+                <span className="tooltiptext">Coming Soon</span>
+              </a>
+              <a href="#" className="cursor-default text-gray-400 tooltip">
+                Roadmap
+                <span className="tooltiptext">Coming Soon</span>
+              </a>
+              <a href="#" className="cursor-default text-gray-400 tooltip">
+                Team
+                <span className="tooltiptext">Coming Soon</span>
+              </a>
+              <a href="#" className="cursor-default text-gray-400 tooltip">
+                DAO
+                <span className="tooltiptext">Coming Soon</span>
+              </a>
+            </div>
+            {/* TODO Mobile Menu */}
+            <div className="flex items-center md:hidden">
+              <button className="focus:outline-none">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
--- src/app/models/links.ts
+++ src/app/models/links.ts
@@ -1,4 +1,5 @@
 export enum SiteLinks {
   twitter = 'https://twitter.com/SuperPhoenixDAO',
   launchVideo = 'https://youtube.com/embed/h6CJYtY3tCw',
+  whitepaper = 'https://shdw-drive.genesysgo.net/BamXrN29Jkq9HA3UyLZWEaikDa3zcVgnP6nakvrZdQ72/whitepaper-v1.pdf',
 }
