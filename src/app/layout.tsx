9dc1df5347abd88654e253b6873eaa94879579d0
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2024-05-10 16:49:43 +0300

    add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { semplicita } from './models/fonts';
 import './globals.css';
+import GoogleAnalytics from './components/GoogleAnalytics';
 
 export const metadata: Metadata = {
   title: 'Super Phoenix DAO',
@@ -14,6 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${semplicita.variable} scroll-smooth font-semplicita font-light`}>
+      <GoogleAnalytics />
       <body className="h-full bg-gray-850">{children}</body>
     </html>
   );
