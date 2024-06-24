ba1d503e684d1c0cbc1347b3d8681b805aea3f8c
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2024-06-24 21:22:58 +0300

    eslint

--- src/app/components/revealModal.tsx
+++ src/app/components/revealModal.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 
 export const TIER_INFO = {
   T1: {
