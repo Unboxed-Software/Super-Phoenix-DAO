fae1cc19e3938892356a790247f80a2586511246
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2024-06-25 18:27:06 +0300

    eslint

--- src/app/components/mintButtonsWrapper.tsx
+++ src/app/components/mintButtonsWrapper.tsx
@@ -2,7 +2,6 @@
 import useUmi from './wallet/useUmi';
 import useCandyMachine from './wallet/useCandyMachine';
 import MintButtons from './mintButtons';
-import { Skeleton } from '@/components/ui/skeleton';
 
 export default function MintButtonsWrapper() {
   const { wallet, umi } = useUmi();
@@ -11,12 +10,6 @@ export default function MintButtonsWrapper() {
   return isLoaded ? (
     <MintButtons wallet={wallet} umi={umi} candyGuard={candyGuard!} />
   ) : (
-
-    <div className="flex flex-col space-y-2">
-      {/* <Skeleton className="h-[40px] w-[300px] rounded-xl" />
-      <Skeleton className="h-[40px] w-[300px] rounded-xl" />
-      <Skeleton className="h-[40px] w-[300px] rounded-xl" />
-      <Skeleton className="h-[40px] w-[300px] rounded-xl" /> */}
-    </div>
+    <div className="flex flex-col space-y-2"></div>
   );
 }
