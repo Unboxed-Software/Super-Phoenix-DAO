0e76002e7feed496ba1db2985aa05b5a42edafbe
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2024-06-25 21:03:26 +0300

    connect wallet on mint button click if it is disconnected

--- src/app/components/mintButtons.tsx
+++ src/app/components/mintButtons.tsx
@@ -19,22 +19,24 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import { WalletContextState } from '@solana/wallet-adapter-react';
 import { fetchDigitalAsset } from '@metaplex-foundation/mpl-token-metadata';
 import RevealModal, { TIER_INFO } from './revealModal';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import MintCount from './MintCount';
 import CountDown from './landing/CountDown';
+import { useWalletModal } from '@solana/wallet-adapter-react-ui';
 
 dayjs.extend(relativeTime);
 
 const EXPLORER_LINK = process.env.NEXT_PUBLIC_SOLANA_EXPLORER_LINK;
 
 type Props = {
-  candyGuard: CandyGuard;
   umi: Umi;
+  candyGuard: CandyGuard;
   wallet: WalletContextState;
 };
 
 export default function MintButtons({ candyGuard, umi, wallet }: Props) {
   const [isMinting, setIsMinting] = useState(false);
+
+  const { setVisible } = useWalletModal();
 
   const [open, setOpen] = useState(false);
   const [tier, setTier] = useState<keyof typeof TIER_INFO>();
@@ -58,6 +60,11 @@ export default function MintButtons({ candyGuard, umi, wallet }: Props) {
   }, []);
 
   const handleMinting = async (mintingGroup: keyof typeof MINTING_GROUP) => {
+    if (!wallet.publicKey) {
+      setVisible(true);
+      return;
+    }
+
     setIsMinting(true);
     toast({
       title: 'Minting your NFT...',
@@ -149,14 +156,6 @@ export default function MintButtons({ candyGuard, umi, wallet }: Props) {
       <div className="flex w-full justify-center">
         <MintCount mintedCount={mintedCount} />
       </div>
-      {!wallet.connected && (
-        <div className="container mb-6 max-w-[500px]">
-          <Alert variant="attention">
-            <AlertTitle>Attention!</AlertTitle>
-            <AlertDescription>Please Connect your wallet</AlertDescription>
-          </Alert>
-        </div>
-      )}
 
       <div className="container flex flex-col space-y-5 md:w-1/2">
         <div className="flex justify-between align-middle">
@@ -169,7 +168,7 @@ export default function MintButtons({ candyGuard, umi, wallet }: Props) {
               <>
                 <Button
                   isLoading={isMinting}
-                  disabled={!wallet.connected || isMinting || !isFreeMintLive}
+                  disabled={isMinting || !isFreeMintLive}
                   onClick={() => handleMinting(MINTING_GROUP.FL)}
                   variant="gold"
                   className="w-20 appearance-none rounded-md border border-neutral-500 p-3 font-medium leading-tight text-neutral-300"
@@ -192,7 +191,7 @@ export default function MintButtons({ candyGuard, umi, wallet }: Props) {
               <>
                 <Button
                   isLoading={isMinting}
-                  disabled={!wallet.connected || isMinting || !isWhitelistMintLive}
+                  disabled={isMinting || !isWhitelistMintLive}
                   onClick={() => handleMinting(MINTING_GROUP.WLSOL)}
                   variant="gold"
                   className="w-20 appearance-none rounded-md border border-neutral-500 p-3 font-medium leading-tight text-neutral-300"
@@ -201,7 +200,7 @@ export default function MintButtons({ candyGuard, umi, wallet }: Props) {
                 </Button>
                 <Button
                   isLoading={isMinting}
-                  disabled={!wallet.connected || isMinting || !isWhitelistMintLive}
+                  disabled={isMinting || !isWhitelistMintLive}
                   onClick={() => handleMinting(MINTING_GROUP.WLSA)}
                   variant="gold"
                   className="w-20 appearance-none rounded-md border border-neutral-500 p-3 font-medium leading-tight text-neutral-300"
@@ -224,7 +223,7 @@ export default function MintButtons({ candyGuard, umi, wallet }: Props) {
               <>
                 <Button
                   isLoading={isMinting}
-                  disabled={!wallet.connected || isMinting || !isPublicMintLive}
+                  disabled={isMinting || !isPublicMintLive}
                   onClick={() => handleMinting(MINTING_GROUP.PSOL)}
                   variant="gold"
                   className="w-20 appearance-none rounded-md border border-neutral-500 p-3 font-medium leading-tight text-neutral-300"
@@ -233,7 +232,7 @@ export default function MintButtons({ candyGuard, umi, wallet }: Props) {
                 </Button>
                 <Button
                   isLoading={isMinting}
-                  disabled={!wallet.connected || isMinting || !isPublicMintLive}
+                  disabled={isMinting || !isPublicMintLive}
                   onClick={() => handleMinting(MINTING_GROUP.PSA)}
                   variant="gold"
                   className="w-20 appearance-none rounded-md border border-neutral-500 p-3 font-medium leading-tight text-neutral-300"
